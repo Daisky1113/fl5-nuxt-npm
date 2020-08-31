8b23e2c20701dd742e358fe4dcca0e3bdd4894f3
Daisky <nakamura.daisky@gmail.com>
2020-09-01 04:57:58 +0900

    nuxt.config.jsのheadの中のtitleとmetaを変更する

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,9 +17,10 @@ export default {
   */
   head: {
     titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    title: 'Hello World',
     meta: [
       { charset: 'utf-8' },
+      { lang: 'ja-jp' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
