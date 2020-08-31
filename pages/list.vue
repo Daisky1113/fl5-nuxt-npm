b682b920e5c6c89686329bd25a92f28fb81c305e
Daisky <nakamura.daisky@gmail.com>
2020-08-31 20:10:07 +0900

    pagesディレクトリ内に新しいファイルを作る

---
+++ pages/list.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="6">
+        <v-card>
+          <v-card-title>{{msg}}</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    msg: "Hello my list",
+  }),
+  components: {},
+};
+</script>
