0ecf378b534fcfc5682532ec54745999bc4f3c1a
Daisky <nakamura.daisky@gmail.com>
2020-09-01 02:48:49 +0900

    $routeからパラメータを取得する（失敗例）

---
+++ pages/author/_id.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12">
+        <p>{{ msg }}</p>
+        <p>{{ id }}</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  data: () => ({
+    msg: `/author/_id.vueを表示`,
+    id: this.$route.params.id,
+  }),
+};
+</script>
