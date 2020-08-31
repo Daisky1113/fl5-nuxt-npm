5e1e9f2d5a0ca3b8e75d26957999d545a73fd2e8
Daisky <nakamura.daisky@gmail.com>
2020-08-31 20:59:51 +0900

    コンポーネント側でvuexで定義したstateとmutationを読み込む

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,10 @@
     <v-row justify="center">
       <v-col cols="6">
         <v-card>
-          <v-card-title>{{msg}}</v-card-title>
+          <v-card-title>{{message}}</v-card-title>
+          <v-card-text>
+            <v-text-field @input="changeMessage" label="Outlined" outlined></v-text-field>
+          </v-card-text>
         </v-card>
       </v-col>
     </v-row>
@@ -11,10 +14,17 @@
 </template>
 
 <script>
+import { mapState, mapMutations } from "vuex";
 export default {
   data: () => ({
     msg: "Hello World is mine",
   }),
+  computed: {
+    ...mapState(["message"]),
+  },
+  methods: {
+    ...mapMutations(["changeMessage"]),
+  },
   components: {},
 };
 </script>
