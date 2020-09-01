d7e9cac622375bc36cea1a6f5827d73c4de1017a
Daisky <nakamura.daisky@gmail.com>
2020-09-01 11:52:31 +0900

    要素が空のInputFormコンポーネントを作成

---
+++ components/InputForm.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-card>
+    <v-card-text></v-card-text>
+  </v-card>
+</template>
+<script>
+export default {
+  name: "InputForm",
+  data: () => ({}),
+  methods: {},
+};
+</script>
