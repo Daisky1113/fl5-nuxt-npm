d4f186ae6ea548279112f1ca33f2cd9f3592457f
Daisky <nakamura.daisky@gmail.com>
2020-09-01 12:58:08 +0900

    shallowMountからmountに変更

--- test/InputForm.spec.js
+++ test/InputForm.spec.js
@@ -1,28 +1,31 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-import { shallowMount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
 import InputForm from '../components/InputForm.vue'
 
 
 Vue.use(Vuetify)
 
 describe('InputForm', () => {
-  const wrapper = shallowMount(InputForm)
-  const input = wrapper.find('#titleInput')
 
   test('インプットが存在する', () => {
+    const wrapper = mount(InputForm)
+    const input = wrapper.find('#titleInput')
+
     expect(input.exists()).toBe(true)
   })
 
   test('dataにはinputTextフィールドがある', () => {
+    const wrapper = mount(InputForm)
     const inputValue = wrapper.vm.$data.inputTex
     expect(inputValue).toBe('')
   })
 
   test('inputの値が変わるとinputTextの値も変わる', () => {
+    const wrapper = mount(InputForm)
+    const input = wrapper.find('#titleInput')
     input.setValue('hogehoge')
-    const inputVal = wrapper.vm.$data.inputTex
-    expect(inputValue).toBe('hogehoge')
+    expect(wrapper.vm.$data.inputTex).toBe('hogehoge')
   })
 
 })
